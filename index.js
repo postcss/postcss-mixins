dcdcee94badf50897c5b8f08cd8fc00502bdb11b
Andrey Sitnik <andrey@sitnik.ru>
2015-02-01 08:48:07 +0300

    Add base support for function mixins

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-module.exports = function (opts) { };
+var postcss = require('postcss');
+var list    = require('postcss/lib/list');
+
+var insertMixin = function (mixins, rule, opts) {
+    var params = list.space(rule.params);
+    var name   = params.shift();
+    var mixin  = mixins[name];
+
+    if ( !mixin ) {
+        if ( !opts.silent ) {
+            throw rule.error('Undefined mixin ' + name);
+        }
+    } else if ( typeof(mixin) == 'function' ) {
+        var args = [rule].concat(params);
+        mixin.apply(this, args);
+    }
+
+    if ( rule.parent ) rule.removeSelf();
+};
+
+module.exports = function (opts) {
+    if ( typeof(opts) == 'undefined' ) opts = { };
+
+    var mixins = { };
+    if ( typeof(opts.mixins) == 'object' ) {
+        for ( var i in opts.mixins ) mixins[i] = opts.mixins[i];
+    }
+
+    return function (css) {
+        css.eachAtRule(function (rule) {
+
+            if ( rule.name == 'mixin' ) {
+                insertMixin(mixins, rule, opts);
+            }
+
+        });
+    }
+};
 
 module.exports.postcss = function (css) {
     module.exports()(css);
--- test/mixins.js
+++ test/mixins.js
@@ -3,4 +3,38 @@ var expect  = require('chai').expect;
 
 var mixins = require('../');
 
-describe('postcss-mixins', function () { });
+var test = function (input, output, opts) {
+    expect(postcss(mixins(opts)).process(input).css).to.eql(output);
+};
+
+describe('postcss-mixins', function () {
+
+    it('throws error on unknown mixin', function () {
+        expect(function () {
+            test('@mixin A');
+        }).to.throw('Undefined mixin A');
+    });
+
+    it('cans remove unknown mixin on request', function () {
+        test('@mixin A; a{}', 'a{}', { silent: true });
+    });
+
+    it('supports functions mixins', function () {
+        test('a { @mixin color black; }', 'a { color: black; }', {
+            mixins: {
+                color: function (rule, color) {
+                    rule.replaceWith({ prop: 'color', value: color });
+                }
+            }
+        });
+    });
+
+    it('removes mixin at-rule', function () {
+        test('a { @mixin none; }', 'a { }', {
+            mixins: {
+                none: function (rule) { }
+            }
+        });
+    })
+
+});
