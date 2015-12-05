944a9e171d3a97b394fffe70541cd0d33e2039c9
Andrey Sitnik <andrey@sitnik.ru>
2015-12-05 21:00:29 +0100

    Replace tests to ava

--- test/test.js
+++ test/test.js
@@ -1,180 +1,166 @@
-var postcss = require('postcss');
-var expect  = require('chai').expect;
-var path    = require('path');
+import postcss from 'postcss';
+import path    from 'path';
+import test    from 'ava';
 
-var mixins = require('../');
+import mixins from '../';
 
-var test = function (input, output, done, opts) {
-    return postcss(mixins(opts)).process(input).then(function (result) {
-        expect(result.css).to.eql(output);
-        expect(result.warnings()).to.be.empty;
-        done();
+function run(t, input, output, opts) {
+    return postcss(mixins(opts)).process(input).then(result => {
+        t.same(result.css, output);
+        t.same(result.warnings().length, 0);
     });
-};
+}
 
-describe('postcss-mixins', function () {
-
-    it('throws error on unknown mixin', function () {
-        test('@mixin A').catch(function (err) {
-            expect(err.name).to.eql('Undefined mixin A');
-        });
+test('throws error on unknown mixin', t => {
+    return postcss(mixins).process('@mixin A').catch(err => {
+        t.same(err.reason, 'Undefined mixin A');
     });
+});
 
-    it('cans remove unknown mixin on request', function (done) {
-        test('@mixin A; a{}', 'a{}', done, { silent: true });
-    });
+test('cans remove unknown mixin on request', t => {
+    return run(t, '@mixin A; a{}', 'a{}', { silent: true });
+});
 
-    it('supports functions mixins', function (done) {
-        test('a { @mixin color black; }', 'a { color: black; }', done, {
-            mixins: {
-                color: function (rule, color) {
-                    rule.replaceWith({ prop: 'color', value: color });
-                }
+test('supports functions mixins', t => {
+    return run(t, 'a { @mixin color black; }', 'a { color: black; }', {
+        mixins: {
+            color: (rule, color) => {
+                rule.replaceWith({ prop: 'color', value: color });
             }
-        });
+        }
     });
+});
 
-    it('removes mixin at-rule', function (done) {
-        test('a { @mixin none; }', 'a { }', done, {
-            mixins: {
-                none: function () { }
+test('removes mixin at-rule', t => {
+    return run(t, 'a { @mixin none; }', 'a { }', {
+        mixins: {
+            none: () => { }
+        }
+    });
+});
+
+test('converts object from function to nodes', t => {
+    return run(t, 'a { @mixin color black; }', 'a { color: black; }', {
+        mixins: {
+            color: (rule, color) => {
+                return { color: color };
             }
-        });
+        }
     });
+});
 
-    it('converts object from function to nodes', function (done) {
-        test('a { @mixin color black; }', 'a { color: black; }', done, {
+test('supports object mixins', t => {
+    return run(t, '@mixin obj;',
+        '@media screen {\n    b {\n        one: 1\n    }\n}', {
             mixins: {
-                color: function (rule, color) {
-                    return { color: color };
-                }
-            }
-        });
-    });
-
-    it('supports object mixins', function (done) {
-        test('@mixin obj;',
-            '@media screen {\n    b {\n        one: 1\n    }\n}', done, {
-                mixins: {
-                    obj: {
-                        '@media screen': {
-                            b: {
-                                one: 1
-                            }
+                obj: {
+                    '@media screen': {
+                        b: {
+                            one: 1
                         }
                     }
                 }
-            });
-    });
-
-    it('supports CSS mixins', function (done) {
-        test('@define-mixin black { color: black; } a { @mixin black; }',
-             'a { color: black; }',
-             done);
-    });
-
-    it('uses variable', function (done) {
-        test('@define-mixin color $color { color: $color $other; } ' +
-             'a { @mixin color black; }',
-             'a { color: black $other; }',
-             done);
-    });
-
-    it('supports default value', function (done) {
-        test('@define-mixin c $color: black { color: $color; } a { @mixin c; }',
-             'a { color: black; }',
-             done);
-    });
-
-    it('supports mixins with content', function (done) {
-        test('@define-mixin m { @media { @mixin-content; } } @mixin m { a {} }',
-             '@media {\n    a {}\n}',
-             done);
-    });
-
-    it('uses variables', function (done) {
-        test('@define-mixin m $a, $b: b, $c: c { v: $a $b $c; } @mixin m 1, 2;',
-             'v: 1 2 c;',
-             done);
-    });
-
-    it('loads mixins from dir', function (done) {
-        test(
-            'a { @mixin a 1; @mixin b; @mixin d; }',
-            'a { a: 1; b: 2; d: 4; }',
-            done,
-            {
-                mixinsDir: path.join(__dirname, 'mixins')
             }
-        );
-    });
+        });
+});
 
-    it('loads mixins from relative dir', function (done) {
-        test(
-            'a { @mixin a 1; @mixin b; @mixin d; }',
-            'a { a: 1; b: 2; d: 4; }',
-            done,
-            {
-                mixinsDir: 'test/mixins/'
+test('supports CSS mixins', t => {
+    return run(t, '@define-mixin black { color: black; } a { @mixin black; }',
+                  'a { color: black; }');
+});
+
+test('uses variable', t => {
+    return run(t, '@define-mixin color $color { color: $color $other; } ' +
+                  'a { @mixin color black; }',
+                  'a { color: black $other; }');
+});
+
+test('supports default value', t => {
+    return run(t, '@define-mixin c $color: black { color: $color; } ' +
+                  'a { @mixin c; }',
+                  'a { color: black; }');
+});
+
+test('supports mixins with content', t => {
+    return run(t, '@define-mixin m { @media { @mixin-content; } } ' +
+                  '@mixin m { a {} }',
+                  '@media {\n    a {}\n}');
+});
+
+test('uses variables', t => {
+    return run(t, '@define-mixin m $a, $b: b, $c: c { v: $a $b $c; }' +
+                  '@mixin m 1, 2;',
+                  'v: 1 2 c;');
+});
+
+test('loads mixins from dir', t => {
+    return run(t,
+        'a { @mixin a 1; @mixin b; @mixin d; }',
+        'a { a: 1; b: 2; d: 4; }',
+        {
+            mixinsDir: path.join(__dirname, 'mixins')
+        }
+    );
+});
+
+test('loads mixins from relative dir', t => {
+    return run(t,
+        'a { @mixin a 1; @mixin b; @mixin d; }',
+        'a { a: 1; b: 2; d: 4; }',
+        {
+            mixinsDir: 'mixins/'
+        }
+    );
+});
+
+test('loads mixins from dirs', t => {
+    return run(t, 'a { @mixin a 1; @mixin c; }', 'a { a: 1; c: 3; }', {
+        mixinsDir: [
+            path.join(__dirname, 'mixins'),
+            path.join(__dirname, 'other')
+        ]
+    });
+});
+
+test('loads mixins from relative dirs', t => {
+    return run(t, 'a { @mixin a 1; @mixin c; }', 'a { a: 1; c: 3; }', {
+        mixinsDir: ['mixins', 'other']
+    });
+});
+
+test('loads mixins from file glob', t => {
+    return run(t, 'a { @mixin a 1; @mixin b; }', 'a { a: 1; b: 2; }', {
+        mixinsFiles: path.join(__dirname, 'mixins', '*.{js,json}')
+    });
+});
+
+test('loads mixins from file globs', t => {
+    return run(t, 'a { @mixin a 1; @mixin c; }', 'a { a: 1; c: 3; }', {
+        mixinsFiles: [
+            path.join(__dirname, 'mixins', '*.!(json|css)'),
+            path.join(__dirname, 'other', '*')
+        ]
+    });
+});
+
+test('coverts mixins values', t => {
+    var proccessor = postcss(mixins({
+        mixins: {
+            empty: () => {
+                return { width: 0 };
             }
-        );
+        }
+    }));
+    return proccessor.process('a{ @mixin empty; }').then(result => {
+        t.same(typeof result.root.first.first.value, 'string');
     });
+});
 
-    it('loads mixins from dirs', function (done) {
-        test('a { @mixin a 1; @mixin c; }', 'a { a: 1; c: 3; }', done, {
-            mixinsDir: [
-                path.join(__dirname, 'mixins'),
-                path.join(__dirname, 'other')
-            ]
-        });
+test('supports deprecated variables syntax', t => {
+    return postcss(mixins).process(
+        '@define-mixin m $a $b $c { v: $a $b $c; } @mixin m 1 2 3;'
+    ).then(result => {
+        t.same(result.css, 'v: 1 2 3;');
+        t.same(result.warnings().length, 2);
     });
-
-    it('loads mixins from dirs', function (done) {
-        test('a { @mixin a 1; @mixin c; }', 'a { a: 1; c: 3; }', done, {
-            mixinsDir: [
-                'test/mixins',
-                'test/other'
-            ]
-        });
-    });
-
-    it('loads mixins from file glob', function (done) {
-        test('a { @mixin a 1; @mixin b; }', 'a { a: 1; b: 2; }', done, {
-            mixinsFiles: path.join(__dirname, 'mixins', '*.{js,json}')
-        });
-    });
-
-    it('loads mixins from file globs', function (done) {
-        test('a { @mixin a 1; @mixin c; }', 'a { a: 1; c: 3; }', done, {
-            mixinsFiles: [
-                path.join(__dirname, 'mixins', '*.!(json|css)'),
-                path.join(__dirname, 'other', '*')
-            ]
-        });
-    });
-
-    it('coverts mixins values', function (done) {
-        var proccessor = postcss(mixins({
-            mixins: {
-                empty: function () {
-                    return { width: 0 };
-                }
-            }
-        }));
-        proccessor.process('a{ @mixin empty; }').then(function (result) {
-            expect(result.root.first.first.value).to.be.a('string');
-            done();
-        });
-    });
-
-    it('supports deprecated variables syntax', function (done) {
-        postcss(mixins).process(
-            '@define-mixin m $a $b $c { v: $a $b $c; } @mixin m 1 2 3;'
-        ).then(function (result) {
-            expect(result.css).to.eql('v: 1 2 3;');
-            expect(result.warnings()).to.have.length(2);
-            done();
-        });
-    });
-
 });
